94c77f831e5dfd3466457693f2c9f15305beccf5
Hugo <canohuginho@egmail.com>
2021-01-15 14:59:46 -0600

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,16 @@
 //Puerto
 process.env.PORT = process.env.PORT || 3000;
+
+//Entorno
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//Base de datos
+let urlBD;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlBD = 'mongodb://localhost:27017/cafe';
+} else {
+    urlBD = 'mongodb+srv://admin:7bTaLoUahW7kG74Y@cluster0.5ux0c.mongodb.net/Cafe'; //canohuginho:1.2,gfda
+}
+
+process.env.urlBD = urlBD;
