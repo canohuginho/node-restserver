b59ff002092f2912087d6044c0efd1d2f27984e9
Hugo <canohuginho@egmail.com>
2021-01-15 16:43:27 -0600

    RestServer - usuario variables de entorno

--- server/config/config.js
+++ server/config/config.js
@@ -10,7 +10,7 @@ let urlBD;
 if (process.env.NODE_ENV === 'dev') {
     urlBD = 'mongodb://localhost:27017/cafe';
 } else {
-    urlBD = 'mongodb+srv://admin:7bTaLoUahW7kG74Y@cluster0.5ux0c.mongodb.net/Cafe'; //canohuginho:1.2,gfda
+    urlBD = process.env.MONGO_URL //'mongodb+srv://admin:7bTaLoUahW7kG74Y@cluster0.5ux0c.mongodb.net/Cafe'; //canohuginho:1.2,gfda
 }
 
 process.env.urlBD = urlBD;
